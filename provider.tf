b37551431b347fb6b0f1ee60d758baac46b250a9
Hareesh5041 <82581268+Hareesh5041@users.noreply.github.com>
2021-06-14 13:26:50 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,15 +1,6 @@
 provider "google" {
-  # credentials are inferred from the environment through `gcloud auth application-default login`
-  # credentials = file("google_cloud_credentials.json")
-
-  # optional, may also be configured via `gcloud config set project <project ID>`
-  project = var.project_id
-
-  # see https://cloud.google.com/compute/docs/regions-zones for more information
-  region = var.project_region
-}
-
-provider "google-beta" {
+  version = "~> 3.67.0"
   project = var.project_id
   region  = var.project_region
+  credentials = file("credentials.json")
 }
